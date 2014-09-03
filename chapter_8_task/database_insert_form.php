bbc11cd1ebdb13c1c906b1dbe3e64589ecbfe59f
neo0057 <neo0057@users.noreply.github.com>
2014-09-03 11:19:21 +0530

    update database_insert_form.php

--- chapter_8_task/database_insert_form.php
+++ chapter_8_task/database_insert_form.php
@@ -1,36 +1,11 @@
-<?php
+<body>
 
-require_once("DBConnection.php");
+	<form id='database_insert_form' action='database_insert.php' method='post' accept-charset='UTF-8'>
+	CouponCode:  <input type='text' name='couponcode'/>
+	Percentage Off: <input type='text' name='percentageoff'/>
+	<input type='Submit' name='Insert'/>
+	<?php
+		require_once("database_insert.php");
+	?> 
 
-/**
-* Database Insertion class
-*/
-class DBInsert
-{
-	function build_query($coupon_code, $percentageoff, $table_name = 'coupon')
-	{
-		$query = "insert into $table_name values('{$coupon_code}', $percentageoff)";
-		return $query;
-	}
-	function insert($query)
-	{
-		$obj = new DBConnection();
-		$db = $obj->connect();
-		mysqli_query($db, $query) or die(mysqli_error());
-		echo "data successfully inserted <br>";
-		$obj->disconnect();
-	}
-}
-
-	$db_insert_object = new DBInsert();
-
-	$coupon_code = $_POST['couponcode'];
-	$off_value_int = 0; // default value is 0
-	if (is_numeric(trim($_POST['percentageoff']))) {
-		$off_value_int = (int)$_POST['percentageoff'];
-	}
-
-	$query = $db_insert_object->build_query($coupon_code, $off_value_int);
-	$db_insert_object->insert($query);
-
-?>
+</body>
